483be2a7c6ae80c2c98fe1c32284886ea89ac299
Manish Ranjan <manish.ranjan@gameskraft.com>
2023-03-03 22:51:49 +0530

    Initial commit

---
+++ src/utils/motion.ts
@@ -0,0 +1,102 @@
+export const textVariant = (delay: number) => {
+  return {
+    hidden: {
+      y: -50,
+      opacity: 0,
+    },
+    show: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        duration: 1.25,
+        delay: delay,
+      },
+    },
+  };
+};
+
+export const fadeIn = (direction: string, type: string, delay: number, duration: number) => {
+  const directionValue: Record<string, number> = {
+    left: 100,
+    right: -100,
+    up: 100,
+    down: -100,
+  }
+  return {
+    hidden: {
+      x: directionValue?.[direction] || 0,
+      y: directionValue?.[direction] || 0,
+      opacity: 0,
+    },
+    show: {
+      x: 0,
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: type,
+        delay: delay,
+        duration: duration,
+        ease: "easeOut",
+      },
+    },
+  };
+};
+
+export const zoomIn = (delay: number, duration: number) => {
+  return {
+    hidden: {
+      scale: 0,
+      opacity: 0,
+    },
+    show: {
+      scale: 1,
+      opacity: 1,
+      transition: {
+        type: "tween",
+        delay: delay,
+        duration: duration,
+        ease: "easeOut",
+      },
+    },
+  };
+};
+
+export const slideIn = (direction: string, type: string, delay: string, duration: string) => {
+
+  const directionValue: Record<string, string> = {
+    left: "-100%",
+    right: "100%",
+    up: "100%",
+    down: "100%",
+  }
+
+  return {
+    hidden: {
+      x: directionValue?.[direction] || 0,
+      y: directionValue?.[direction] || 0,
+    },
+    show: {
+      x: 0,
+      y: 0,
+      transition: {
+        type: type,
+        delay: delay,
+        duration: duration,
+        ease: "easeOut",
+      },
+    },
+  };
+};
+
+export const staggerContainer = (staggerChildren: any, delayChildren: number) => {
+  return {
+    hidden: {},
+    show: {
+      transition: {
+        staggerChildren: staggerChildren,
+        delayChildren: delayChildren || 0,
+      },
+    },
+  };
+};
